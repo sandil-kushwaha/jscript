828538a207dee03c586940a1cfdf315ba331e030
sandil-kushwaha <ss7kushwaha@gmail.com>
2024-08-07 12:37:04 +0000

    base

---
+++ basics_01/01_variables.js
@@ -0,0 +1,12 @@
+const account_id=989
+console.log(account_id)
+let mail="ss7"
+var city="patna"
+password=1234
+
+
+//node basics_01/01_variables.jsaccount_id=000
+mail="oikl"
+city="rohtas"
+password="kill"
+console.table([account_id,mail,city,password])
---
+++ basics_01/03_conversion_operations.js
@@ -0,0 +1,7 @@
+let score="iop";
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueinnumber=Number(score)
+console.log(valueinnumber)
+console.log(typeof valueinnumber)
