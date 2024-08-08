e0456be8a0dd8622b68c32511e01f746eb992018
sandil-kushwaha <ss7kushwaha@gmail.com>
2024-08-08 18:13:05 +0000

    maths

---
+++ basics_01/04_trings.js
@@ -0,0 +1,4 @@
+const name="Sandil"
+const age=22
+
+console.log(`hello my name is ${name} and my age is ${age}`)
---
+++ basics_01/05_maths.js
@@ -0,0 +1,12 @@
+const score=100;
+console.log(score);
+
+const newnum=new Number(1000)
+console.log(newnum)
+console.log(newnum.toFixed(3))
+
+console.log(Math.min(9,10,11,12,0,-2,-100))
+console.log(Math.max(9,10,11,12,0,-2,-100))
+const min=50,max=1000;
+console.log(Math.random())
+console.log(Math.floor(Math.random()*(max-min+1))+min);
