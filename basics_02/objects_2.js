26fee5a30d56768797fb8d67e7a0758d61927f9e
sandil-kushwaha <ss7kushwaha@gmail.com>
2024-08-10 18:35:11 +0000

    onjectss

--- basics_02/objects_2.js
+++ basics_02/objects_2.js
@@ -3,5 +3,5 @@ tinder.name="sandil kushwaha"
 tinder.age=22
 tinder.theme="dark"
 tinder.func=function(){
-console.log(`here is all the information ${this.name} ${this.age}${this.theme}`)}
+console.log(`here is all the information ${this.name} ${this.age} ${this.theme}`)}
 console.log(tinder.func())
