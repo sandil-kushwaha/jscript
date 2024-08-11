3ed0c7c9ea8b5f5c909cd9f5ea35cb84c932dc6a
sandil-kushwaha <ss7kushwaha@gmail.com>
2024-08-11 07:52:40 +0000

    destruct

--- basics_02/objects_2.js
+++ basics_02/objects_2.js
@@ -5,3 +5,12 @@ tinder.theme="dark"
 tinder.func=function(){
 console.log(`here is all the information ${this.name} ${this.age} ${this.theme}`)}
 console.log(tinder.func())
+
+const course={
+    nameg:"kushwaha",
+     price:"1000",
+     time:6
+}
+console.log(course)
+const {nameg:kl}=course
+console.log(kl)
