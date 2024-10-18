0f83e1cb702df67fed81b6606c4961debbb97d23
sandil-kushwaha <ss7kushwaha@gmail.com>
2024-10-18 07:19:23 +0000

    html

--- basics_02/arrays_01.js
+++ basics_02/arrays_01.js
@@ -1,6 +1,9 @@
-const myar=[0,1,2,3,4,5,6,7]
-const myar2=new Array(0,1,2,3,4,5,6,67,8,9,10)
-
-const newjk=myar2.splice(1,4);
-console.log(newjk);
-console.log(myar2);
+for(let i=0;i<=10;i++)
+  {
+     console.log(`outer value of loop: ${i}`);
+      for(let j=0;j<=10;j++)
+       console.log(`outer value ${i} and inner value ${j}`)
+  }
+  const nums=[1,2,3,4,5,6,7,8,9,10];
+  let newnums=nums.filter((num)=>num <8)
+  console.log(newnums);
